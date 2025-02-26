2ed1de1191530d202d0d2357da10ad308d20d3d4
Vazhapp <vkentchiashvili@gmail.com>
2025-02-27 01:17:10 +0400

    add concrete products for Sofa

---
+++ src/refactoring_guru_design_pattenrs/abstractFactory/furnitureCompanySimulator/sofa/ModernSofa.kt
@@ -0,0 +1,7 @@
+package refactoring_guru_design_pattenrs.abstractFactory.furnitureCompanySimulator.sofa
+
+class ModernSofa : Sofa {
+    override fun layOn() {
+        println("Lay on Modern Sofa")
+    }
+}
