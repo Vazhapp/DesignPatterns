163f3885dabc1ff0aba9f4a5e6604544eedf0a84
Vazhapp <vkentchiashvili@gmail.com>
2025-02-27 01:18:57 +0400

    add concrete products for Coffee Table

---
+++ src/refactoring_guru_design_pattenrs/abstractFactory/furnitureCompanySimulator/coffeetable/ModernCoffeeTable.kt
@@ -0,0 +1,7 @@
+package refactoring_guru_design_pattenrs.abstractFactory.furnitureCompanySimulator.coffeetable
+
+class ModernCoffeeTable : CoffeeTable {
+    override fun putTheCup() {
+        println("Put the cup on the Modern Coffee Table")
+    }
+}
