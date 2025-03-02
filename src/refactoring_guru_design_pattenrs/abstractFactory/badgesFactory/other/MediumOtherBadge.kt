d2bba4dbc4839888a10df3a950787b6d8dfa21bd
Vazhapp <vkentchiashvili@gmail.com>
2025-03-02 20:00:24 +0400

    Implement other badge products

---
+++ src/refactoring_guru_design_pattenrs/abstractFactory/badgesFactory/other/MediumOtherBadge.kt
@@ -0,0 +1,7 @@
+package refactoring_guru_design_pattenrs.abstractFactory.badgesFactory.other
+
+class MediumOtherBadge : OtherBadge {
+    override fun drawOtherBadge() {
+        println("Draw Medium Other badge")
+    }
+}
---
+++ src/refactoring_guru_design_pattenrs/abstractFactory/badgesFactory/other/SmallOtherBadge.kt
@@ -0,0 +1,7 @@
+package refactoring_guru_design_pattenrs.abstractFactory.badgesFactory.other
+
+class SmallOtherBadge : OtherBadge {
+    override fun drawOtherBadge() {
+        println("Draw Small Other badge")
+    }
+}
