3a579c289b4911b1702b7f81f1838536d7c14837
Vazhapp <vkentchiashvili@gmail.com>
2025-03-03 22:50:25 +0400

    implemented usage of pizza builder and director

--- src/refactoring_guru_design_pattenrs/builderPattern/main.kt
+++ src/refactoring_guru_design_pattenrs/builderPattern/main.kt
@@ -1,12 +1,18 @@
 package refactoring_guru_design_pattenrs.builderPattern
 
-import refactoring_guru_design_pattenrs.builderPattern.carBuilder.Client
+import refactoring_guru_design_pattenrs.builderPattern.pizzaBuilder.Pizzeria
 
 fun main() {
-    val builderClient = Client()
+//    val builderClient = Client()
+//
+//    builderClient.buildCarWithDirector()
+//    builderClient.buildCarWithBuilder()
+//
+//    builderClient.buildCarManual()
+//
+    val pizzeria = Pizzeria()
+    val pepperoniPizza = pizzeria.bakePizzaWithDirector(bakePepperoni = true)
+    val supremePizza = pizzeria.bakePizzaWithBuilder(bakePepperoni = false)
 
-    builderClient.buildCarWithDirector()
-    builderClient.buildCarWithBuilder()
-
-    builderClient.buildCarManual()
+    println("Baked Director Pizzas: $pepperoniPizza \nBaked Buidler Pizzas: $supremePizza")
 }
