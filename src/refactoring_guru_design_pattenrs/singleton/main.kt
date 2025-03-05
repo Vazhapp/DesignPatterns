f52a3658a229f74946ac84e481a606d7f6ae1020
Vazhapp <vkentchiashvili@gmail.com>
2025-03-05 23:48:56 +0400

    thread safe singleton init.

--- src/refactoring_guru_design_pattenrs/singleton/main.kt
+++ src/refactoring_guru_design_pattenrs/singleton/main.kt
@@ -1,11 +1,33 @@
 package refactoring_guru_design_pattenrs.singleton
 
 import refactoring_guru_design_pattenrs.singleton.nonThreadSafe.SingletonNonThreadSafe
+import refactoring_guru_design_pattenrs.singleton.threadSafe.SingletonThreadSafe
 
 fun main() {
-    val firstSingleton = SingletonNonThreadSafe.getInstance("First Singleton")
-    val secondSingleton = SingletonNonThreadSafe.getInstance("Second Singleton")
+//    val firstSingleton = SingletonNonThreadSafe.getInstance("First Singleton")
+//    val secondSingleton = SingletonNonThreadSafe.getInstance("Second Singleton")
+//
+//    println(firstSingleton.value)
+//    println(secondSingleton.value)
 
-    println(firstSingleton.value)
-    println(secondSingleton.value)
+    // If its thread safe sohuld return the same value: 2x First Singleton
+    val threadFirst = Thread(ThreadFirst())
+    val threadSecond = Thread(ThreadSecond())
+
+    threadFirst.start()
+    threadSecond.start()
+}
+
+class ThreadFirst : Runnable {
+    override fun run() {
+        val firstSingleton = SingletonThreadSafe.getInstance("First Singleton")
+        println(firstSingleton.value)
+    }
+}
+
+class ThreadSecond : Runnable {
+    override fun run() {
+        val secondSingleton = SingletonThreadSafe.getInstance("Second Singleton")
+        println(secondSingleton.value)
+    }
 }
