ae293fa4ba4028c49c072c858b545fa648ea5e42
Vazhapp <vkentchiashvili@gmail.com>
2025-03-12 00:23:22 +0400

    add Wardrobe enum for dress up example

---
+++ src/refactoring_guru_design_pattenrs/decoratorPattern/dressUpExample/Cloth.kt
@@ -0,0 +1,12 @@
+package refactoring_guru_design_pattenrs.decoratorPattern.dressUpExample
+
+enum class Wardrobe {
+    JEANS,
+    SHORT,
+    T_SHIRT,
+    CAP,
+    JACKET,
+    RAINCOAT,
+    SHOES,
+    SANDALS;
+}
