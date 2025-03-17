05f9d28a6e99796beeb81d7bf3b6cbc0d8d2b082
Vazhapp <vkentchiashvili@gmail.com>
2025-03-18 00:03:28 +0400

    implement LegacyMediaPlayer

---
+++ src/refactoring_guru_design_pattenrs/adapterPattern/mediaPlayer/legacyMediaPlayer/LegacyMediaPlayer.kt
@@ -0,0 +1,7 @@
+package refactoring_guru_design_pattenrs.adapterPattern.mediaPlayer.legacyMediaPlayer
+
+class LegacyMediaPlayer {
+    fun playWav(fileName: String) {
+        println("Playing WAV Files")
+    }
+}
