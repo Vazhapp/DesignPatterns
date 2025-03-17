34e9916e91628fc64dfe8cb76915f4a32168e8f7
Vazhapp <vkentchiashvili@gmail.com>
2025-03-18 00:07:20 +0400

    Comment shapes example in adapter pattern's main

--- src/refactoring_guru_design_pattenrs/adapterPattern/main.kt
+++ src/refactoring_guru_design_pattenrs/adapterPattern/main.kt
@@ -6,20 +6,23 @@ import refactoring_guru_design_pattenrs.adapterPattern.shapesExample.round.Round
 import refactoring_guru_design_pattenrs.adapterPattern.shapesExample.square.SquarePeg
 
 fun main() {
-    val roundHole = RoundHole(radius = 25.0)
-    val roundPeg = RoundPeg(radius = 10.0)
+    /*** This is Shapes example
+     *     val roundHole = RoundHole(radius = 25.0)
+     *     val roundPeg = RoundPeg(radius = 10.0)
+     *
+     *     val smallSquarePeg = SquarePeg(width = 13.0)
+     *     val largeSquarePeg = SquarePeg(width = 45.0)
+     *
+     *     val smallSquarePegAdapter = SquarePegAdapter(squarePeg = smallSquarePeg)
+     *     val largeSquarePegAdapter = SquarePegAdapter(squarePeg = largeSquarePeg)
+     *
+     *     println("Fit status: ${roundHole.fits(roundPeg)}")
+     *
+     *     if (roundHole.fits(smallSquarePegAdapter))
+     *         println("Fitted Small Square Peg")
+     *
+     *     if (!roundHole.fits(largeSquarePegAdapter))
+     *         println("Not Fitter Large Square Peg")
+     */
 
-    val smallSquarePeg = SquarePeg(width = 13.0)
-    val largeSquarePeg = SquarePeg(width = 45.0)
-
-    val smallSquarePegAdapter = SquarePegAdapter(squarePeg = smallSquarePeg)
-    val largeSquarePegAdapter = SquarePegAdapter(squarePeg = largeSquarePeg)
-
-    println("Fit status: ${roundHole.fits(roundPeg)}")
-
-    if (roundHole.fits(smallSquarePegAdapter))
-        println("Fitted Small Square Peg")
-
-    if (!roundHole.fits(largeSquarePegAdapter))
-        println("Not Fitter Large Square Peg")
 }
