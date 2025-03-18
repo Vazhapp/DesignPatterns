15041b62910036b36dc57253daf411f97f1e8d99
Vazhapp <vkentchiashvili@gmail.com>
2025-03-18 23:10:42 +0400

    implement usage of my Bridge adapter pattern in main function

--- src/refactoring_guru_design_pattenrs/bridgePattern/main.kt
+++ src/refactoring_guru_design_pattenrs/bridgePattern/main.kt
@@ -1,2 +1,28 @@
 package refactoring_guru_design_pattenrs.bridgePattern
 
+import refactoring_guru_design_pattenrs.bridgePattern.devices.Radio
+import refactoring_guru_design_pattenrs.bridgePattern.devices.TV
+import refactoring_guru_design_pattenrs.bridgePattern.remotes.StandardRemote
+
+fun main() {
+    val tv = TV()
+    val radio = Radio()
+
+    val standardRemote = StandardRemote(tv)
+
+    standardRemote.powerOn()
+    standardRemote.volumeUp()
+    standardRemote.volumeUp()
+    standardRemote.volumeUp()
+    tv.getStatus()
+
+    val standardRadioRemote = StandardRemote(radio)
+    standardRadioRemote.powerOn()
+    standardRadioRemote.volumeUp()
+    standardRadioRemote.volumeUp()
+    standardRadioRemote.channelNext()
+    standardRadioRemote.channelNext()
+    standardRadioRemote.channelNext()
+    standardRadioRemote.channelNext()
+    radio.getStatus()
+}
