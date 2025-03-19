280a7aa81d8eb68f448cbc555774b6ebe4d1cabe
Vazhapp <vkentchiashvili@gmail.com>
2025-03-20 00:43:09 +0400

    implement text formatter with Decorator Pattern

---
+++ src/refactoring_guru_design_pattenrs/decoratorPattern/textFormatter/FormatText.kt
@@ -0,0 +1,34 @@
+package refactoring_guru_design_pattenrs.decoratorPattern.textFormatter
+
+interface FormatText {
+    fun buildString(): String
+
+    class Prefix(
+        private val prefix: String,
+        private val formatText: FormatText,
+    ) : FormatText {
+        override fun buildString(): String = "$prefix ${formatText.buildString()}"
+    }
+
+    class Sufix(private val sufix: String, private val formatText: FormatText) : FormatText {
+        override fun buildString(): String = "${formatText.buildString()} $sufix"
+    }
+
+    class Uppercase(
+        private val uppercaseText: String,
+        private val formatText: FormatText,
+    ) : FormatText {
+        override fun buildString(): String {
+            return "${uppercaseText.uppercase()} ${formatText.buildString()}"
+        }
+    }
+
+    class Lowercase(
+        private val lowercaseText: String,
+        private val formatText: FormatText,
+    ) : FormatText {
+        override fun buildString(): String {
+            return "${lowercaseText.lowercase()} ${formatText.buildString()}"
+        }
+    }
+}
