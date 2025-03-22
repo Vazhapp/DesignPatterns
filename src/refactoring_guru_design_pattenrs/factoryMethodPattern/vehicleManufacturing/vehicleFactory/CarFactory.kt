d2a0f0a027b67fa5783af792341b4315ed699204
Vazhapp <vkentchiashvili@gmail.com>
2025-03-22 17:23:42 +0400

    FactoryMethod: add some comments for factory classes

--- src/refactoring_guru_design_pattenrs/factoryMethodPattern/vehicleManufacturing/vehicleFactory/CarFactory.kt
+++ src/refactoring_guru_design_pattenrs/factoryMethodPattern/vehicleManufacturing/vehicleFactory/CarFactory.kt
@@ -3,6 +3,10 @@ package refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturi
 import refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturing.vehicle.Car
 import refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturing.vehicle.Vehicle
 
+/**
+ * The main purpose of a factory is not just to create an object reference,
+ * it can also encapsulate important business logic and details related to the object
+ */
 class CarFactory : VehicleFactory {
     override fun getVehicle(): Vehicle = Car()
 
--- src/refactoring_guru_design_pattenrs/factoryMethodPattern/vehicleManufacturing/vehicleFactory/TruckFactory.kt
+++ src/refactoring_guru_design_pattenrs/factoryMethodPattern/vehicleManufacturing/vehicleFactory/TruckFactory.kt
@@ -3,6 +3,10 @@ package refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturi
 import refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturing.vehicle.Truck
 import refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturing.vehicle.Vehicle
 
+/**
+ * The main purpose of a factory is not just to create an object reference,
+ * it can also encapsulate important business logic and details related to the object
+ */
 class TruckFactory : VehicleFactory {
     override fun getVehicle(): Vehicle = Truck()
 
