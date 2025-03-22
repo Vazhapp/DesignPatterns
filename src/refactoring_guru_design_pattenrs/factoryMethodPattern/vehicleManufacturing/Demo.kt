f911cc6b419375562d903ab36abfda45b78b3d17
Vazhapp <vkentchiashvili@gmail.com>
2025-03-22 17:24:41 +0400

    FactoryMethod: implement usage of factories

---
+++ src/refactoring_guru_design_pattenrs/factoryMethodPattern/vehicleManufacturing/Demo.kt
@@ -0,0 +1,15 @@
+package refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturing
+
+import refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturing.vehicleFactory.CarFactory
+import refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturing.vehicleFactory.MotorcycleFactory
+import refactoring_guru_design_pattenrs.factoryMethodPattern.vehicleManufacturing.vehicleFactory.TruckFactory
+
+fun main() {
+    val carFactory = CarFactory()
+    val motorcycleFactory = MotorcycleFactory()
+    val truckFactory = TruckFactory()
+
+    carFactory.startEngine()
+    motorcycleFactory.startEngine()
+    truckFactory.startEngine()
+}
