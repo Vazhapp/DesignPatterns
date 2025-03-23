5e3e0d2f1142e41fd0220135e6b60d2d743bacb5
Vazhapp <vkentchiashvili@gmail.com>
2025-03-23 16:10:32 +0400

    AbstractFactory: add interior types

---
+++ src/refactoring_guru_design_pattenrs/abstractFactory/carParts/interior/Interior.kt
@@ -0,0 +1,5 @@
+package refactoring_guru_design_pattenrs.abstractFactory.carParts.interior
+
+interface Interior {
+    fun installInterior()
+}
---
+++ src/refactoring_guru_design_pattenrs/abstractFactory/carParts/interior/RuggedInterior.kt
@@ -0,0 +1,7 @@
+package refactoring_guru_design_pattenrs.abstractFactory.carParts.interior
+
+class RuggedInterior : Interior {
+    override fun installInterior() {
+        println("Installed Rugged Interior")
+    }
+}
