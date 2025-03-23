7c1fc6c92defde5d44e5566abf7db9a4f4688b81
Vazhapp <vkentchiashvili@gmail.com>
2025-03-23 16:20:55 +0400

    AbstractFactory: minor function name change in Engine products

--- src/refactoring_guru_design_pattenrs/abstractFactory/carParts/engine/StandardEngine.kt
+++ src/refactoring_guru_design_pattenrs/abstractFactory/carParts/engine/StandardEngine.kt
@@ -1,7 +1,7 @@
 package refactoring_guru_design_pattenrs.abstractFactory.carParts.engine
 
 class StandardEngine : Engine {
-    override fun start() {
+    override fun installEngine() {
         println("Started Standard Engine")
     }
 }
--- src/refactoring_guru_design_pattenrs/abstractFactory/carParts/engine/TurboEngine.kt
+++ src/refactoring_guru_design_pattenrs/abstractFactory/carParts/engine/TurboEngine.kt
@@ -1,7 +1,7 @@
 package refactoring_guru_design_pattenrs.abstractFactory.carParts.engine
 
 class TurboEngine : Engine {
-    override fun start() {
+    override fun installEngine() {
         println("Started Turbo Engine")
     }
 }
