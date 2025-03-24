bed9a6144186684f357df240f3019355784165da
Vazhapp <vkentchiashvili@gmail.com>
2025-03-24 23:30:47 +0400

    Builder: delete main

---
+++ src/refactoring_guru_design_pattenrs/builderPattern/pcBuilder/builder/PersonalComputer.kt
@@ -0,0 +1,29 @@
+package refactoring_guru_design_pattenrs.builderPattern.pcBuilder.builder
+
+class PersonalComputer private constructor(
+    val cpu: String,
+    val gpu: String,
+    val ram: Int,
+    val hdd: Int?,
+    val ssd: Int?,
+    val motherBoard: String,
+) {
+    class Builder {
+        private var cpu = ""
+        private var gpu = ""
+        private var ram = 0
+        private var hdd: Int? = null
+        private var ssd: Int? = 512
+        private var motherBoard = ""
+
+        fun setCpu(cpu: String) = apply { this.cpu = cpu }
+        fun setGpu(gpu: String) = apply { this.gpu = gpu }
+        fun setRam(ram: Int) = apply { this.ram = ram }
+        fun setHdd(hdd: Int?) = apply { this.hdd = hdd }
+        fun setSsd(ssd: Int?) = apply { this.ssd = ssd }
+
+        fun build() = PersonalComputer(
+            cpu, gpu, ram, hdd, ssd, motherBoard
+        )
+    }
+}
