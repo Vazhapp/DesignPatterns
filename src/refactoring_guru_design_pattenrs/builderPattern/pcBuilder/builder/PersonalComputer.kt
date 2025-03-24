e640c347db71be910c7d22c1bf92abe992a74185
Vazhapp <vkentchiashvili@gmail.com>
2025-03-24 23:36:05 +0400

    Builder: implement default personal computer values

--- src/refactoring_guru_design_pattenrs/builderPattern/pcBuilder/builder/PersonalComputer.kt
+++ src/refactoring_guru_design_pattenrs/builderPattern/pcBuilder/builder/PersonalComputer.kt
@@ -9,12 +9,12 @@ class PersonalComputer private constructor(
     val motherBoard: String,
 ) {
     class Builder {
-        private var cpu = ""
-        private var gpu = ""
-        private var ram = 0
+        private var cpu = "I7 12400F"
+        private var gpu = "GTX 1080 TI"
+        private var ram = 16
         private var hdd: Int? = null
         private var ssd: Int? = 512
-        private var motherBoard = ""
+        private var motherBoard = "Z730"
 
         fun setCpu(cpu: String) = apply { this.cpu = cpu }
         fun setGpu(gpu: String) = apply { this.gpu = gpu }
