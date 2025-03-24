86efbe8e9620b7fab53a8e785c308608f4ed233a
Vazhapp <vkentchiashvili@gmail.com>
2025-03-24 23:45:22 +0400

    Builder: implement usage of PersonalComputerBuilder

--- src/refactoring_guru_design_pattenrs/builderPattern/Demo.kt
+++ src/refactoring_guru_design_pattenrs/builderPattern/Demo.kt
@@ -1,5 +1,14 @@
 package refactoring_guru_design_pattenrs.builderPattern
 
-fun main() {
+import refactoring_guru_design_pattenrs.builderPattern.pcBuilder.builder.PersonalComputer
 
+fun main() {
+    val personalComputer = PersonalComputer.Builder()
+        .setCpu("I9 14400K")
+        .setGpu("RTX 3070 TI")
+        .setRam(32)
+        .setSsd(1000)
+        .build()
+
+    println("PC: ${personalComputer.cpu} / ${personalComputer.gpu} / ${personalComputer.ram} / ${personalComputer.hdd} / ${personalComputer.ssd} / ${personalComputer.motherBoard}")
 }
