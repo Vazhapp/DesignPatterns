ed866cd946bb1dd6c773773fa365998bf9ee19ed
Vazhapp <vkentchiashvili@gmail.com>
2025-03-27 23:11:21 +0400

    Bridge: comment Remote control example

--- src/refactoring_guru_design_pattenrs/bridgePattern/main.kt
+++ src/refactoring_guru_design_pattenrs/bridgePattern/main.kt
@@ -5,24 +5,27 @@ import refactoring_guru_design_pattenrs.bridgePattern.devices.TV
 import refactoring_guru_design_pattenrs.bridgePattern.remotes.StandardRemote
 
 fun main() {
-    val tv = TV()
-    val radio = Radio()
+    /**
+     *     val tv = TV()
+     *     val radio = Radio()
+     *
+     *     val standardRemote = StandardRemote(tv)
+     *
+     *     standardRemote.powerOn()
+     *     standardRemote.volumeUp()
+     *     standardRemote.volumeUp()
+     *     standardRemote.volumeUp()
+     *     tv.getStatus()
+     *
+     *     val standardRadioRemote = StandardRemote(radio)
+     *     standardRadioRemote.powerOn()
+     *     standardRadioRemote.volumeUp()
+     *     standardRadioRemote.volumeUp()
+     *     standardRadioRemote.channelNext()
+     *     standardRadioRemote.channelNext()
+     *     standardRadioRemote.channelNext()
+     *     standardRadioRemote.channelNext()
+     *     radio.getStatus()
+     */
 
-    val standardRemote = StandardRemote(tv)
-
-    standardRemote.powerOn()
-    standardRemote.volumeUp()
-    standardRemote.volumeUp()
-    standardRemote.volumeUp()
-    tv.getStatus()
-
-    val standardRadioRemote = StandardRemote(radio)
-    standardRadioRemote.powerOn()
-    standardRadioRemote.volumeUp()
-    standardRadioRemote.volumeUp()
-    standardRadioRemote.channelNext()
-    standardRadioRemote.channelNext()
-    standardRadioRemote.channelNext()
-    standardRadioRemote.channelNext()
-    radio.getStatus()
 }
