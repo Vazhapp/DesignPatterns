eaa2c867682288453ce5cf994744d6f238069604
Vazhapp <vkentchiashvili@gmail.com>
2025-03-27 23:10:41 +0400

    Bridge: optimize imports

--- src/refactoring_guru_design_pattenrs/bridgePattern/notificationAndSender/abstraction/Discord.kt
+++ src/refactoring_guru_design_pattenrs/bridgePattern/notificationAndSender/abstraction/Discord.kt
@@ -1,8 +1,5 @@
 package refactoring_guru_design_pattenrs.bridgePattern.notificationAndSender.abstraction
 
-import refactoring_guru_design_pattenrs.bridgePattern.notificationAndSender.implementation.MessageChannels
-import refactoring_guru_design_pattenrs.bridgePattern.notificationAndSender.implementation.MessageSender
-
 class Discord : Notification() {
     override fun notify(text: String) {
         println("Notify $text")
