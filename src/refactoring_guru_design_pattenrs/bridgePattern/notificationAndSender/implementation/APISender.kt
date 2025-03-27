3e037b018ff2080e78ae1a67374d7da69aa8f764
Vazhapp <vkentchiashvili@gmail.com>
2025-03-27 23:18:11 +0400

    Bridge: minor updates on implementation side

--- src/refactoring_guru_design_pattenrs/bridgePattern/notificationAndSender/implementation/APISender.kt
+++ src/refactoring_guru_design_pattenrs/bridgePattern/notificationAndSender/implementation/APISender.kt
@@ -1,7 +1,5 @@
 package refactoring_guru_design_pattenrs.bridgePattern.notificationAndSender.implementation
 
 class APISender : MessageSender {
-    override fun sendMessageViaChannel(channel: MessageChannels) {
-        println("Send message via ${channel.name}")
-    }
+    override fun sendMessageViaChannel(channel: MessageChannels): String = "Send message via ${channel.name}"
 }
--- src/refactoring_guru_design_pattenrs/bridgePattern/notificationAndSender/implementation/LocalSender.kt
+++ src/refactoring_guru_design_pattenrs/bridgePattern/notificationAndSender/implementation/LocalSender.kt
@@ -1,7 +1,5 @@
 package refactoring_guru_design_pattenrs.bridgePattern.notificationAndSender.implementation
 
 class LocalSender : MessageSender {
-    override fun sendMessageViaChannel(channel: MessageChannels) {
-        println("Send message via ${channel.name}")
-    }
+    override fun sendMessageViaChannel(channel: MessageChannels): String = "Send message via ${channel.name}"
 }
