2d2d959a10875d3f92819043e3122c663a2292ac
Vazhapp <vkentchiashvili@gmail.com>
2025-04-09 18:13:01 +0400

    chain-of-responsibility: implement usage of supports in chain

--- src/refactoring_guru_design_pattenrs/behavioralDesignPatterns/chainOfResposibility/Demo.kt
+++ src/refactoring_guru_design_pattenrs/behavioralDesignPatterns/chainOfResposibility/Demo.kt
@@ -1,5 +1,26 @@
 package refactoring_guru_design_pattenrs.behavioralDesignPatterns.chainOfResposibility
 
-fun main() {
+import refactoring_guru_design_pattenrs.behavioralDesignPatterns.chainOfResposibility.support.LeveThreeSupport
+import refactoring_guru_design_pattenrs.behavioralDesignPatterns.chainOfResposibility.support.LevelOneSupport
+import refactoring_guru_design_pattenrs.behavioralDesignPatterns.chainOfResposibility.support.LevelTwoSupport
+import refactoring_guru_design_pattenrs.behavioralDesignPatterns.chainOfResposibility.support.SupportRequest
 
+fun main() {
+    val levelOneSupport = LevelOneSupport()
+    val levelTwoSupport = LevelTwoSupport()
+    val levelThreeSupport = LeveThreeSupport()
+
+    levelOneSupport.setNext(levelTwoSupport).setNext(levelThreeSupport)
+
+    val supportRequests = listOf(
+        SupportRequest("Printer not working", 2),
+        SupportRequest("Wi-Fi is slow", 5),
+        SupportRequest("Server crashed", 9),
+        SupportRequest("AI takeover imminent", 12)
+    )
+
+    supportRequests.forEach { request ->
+        println("Processing ${request.requestDescription} Complexity: ${request.requestComplexity}")
+        levelOneSupport.handleRequest(request)
+    }
 }
