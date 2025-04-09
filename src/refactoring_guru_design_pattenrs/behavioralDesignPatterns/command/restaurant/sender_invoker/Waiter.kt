2b16e8fe4d3e8bd019baef90dfa6048e086e2447
Vazhapp <vkentchiashvili@gmail.com>
2025-04-10 01:25:33 +0400

    command: implement Waiter -> Sender(invoker) part in this pattern

---
+++ src/refactoring_guru_design_pattenrs/behavioralDesignPatterns/command/restaurant/sender_invoker/Waiter.kt
@@ -0,0 +1,12 @@
+package refactoring_guru_design_pattenrs.behavioralDesignPatterns.command.restaurant.sender_invoker
+
+import refactoring_guru_design_pattenrs.behavioralDesignPatterns.command.restaurant.command.Command
+
+class Waiter {
+    private val orderHistory = mutableListOf<Command>()
+
+    fun placeOrder(command: Command) {
+        command.execute()
+        orderHistory.add(command)
+    }
+}
